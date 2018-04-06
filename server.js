6f5a8d565b2a3a1763c8717662d337ce7611f08a
mash <paullopez05@gmail.com>
2018-04-05 22:56:59 -0700

    add comments to step through code

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+// require files for filesystem and gmagick
 var fs = require('fs')
   , gm = require('gm').subClass({imageMagick: true});
 
+// iterate through images directory to read file names to array
 var files = fs.readdirSync('images/');
+
+// get array count and iterate through each file
+// to create a thumbnail of each image in thumbs directory
 for (let i = 0; i < files.length; i++) {
   gm('images/' + files[i])
   .resize(10, 10, '%')
@@ -9,5 +14,4 @@ for (let i = 0; i < files.length; i++) {
     if(err) return console.dir(arguments)
     console.log(this.outname + "create :: " + arguments[3])
   })
-  
 }
