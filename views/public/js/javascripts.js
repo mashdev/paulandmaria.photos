eb6f723154f97f84f2b04a21d9feeee4aaae5f4a
Paul Lopez <paullopez05@gmail.com>
2018-05-30 22:16:08 -0700

    Added sticky button js code

---
+++ views/public/js/javascripts.js
@@ -0,0 +1,18 @@
+window.onscroll = function() {
+  stickyHeader()
+};
+
+var header = document.getElementById('stickyTop');
+var col = document.getElementById('row-mobile');
+
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if(window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+    col.classList.add("sticky-gap");
+  } else {
+    header.classList.remove("sticky");
+    col.classList.remove("sticky-gap");
+  }
+}
