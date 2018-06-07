01f2dfff19f43a09f394b5dde6076eb2a918e94e
Paul Lopez <paullopez05@gmail.com>
2018-06-07 11:40:35 -0700

    Added test functions for future addon.

--- server.js
+++ server.js
@@ -4,15 +4,15 @@ gm = require('gm').subClass({imageMagick: true}),
 mysql = require('mysql');
 
 // iterate through images directory to read file names to array
-var files = fs.readdirSync('images/');
-
-var resizeLocation = "views/public/webimages/";
-
-gm('images/IMG_0697.JPG')
-.identify(function (err, data) {
-  if (!err) console.log(data)
-});
-
+// var files = fs.readdirSync('images/');
+//
+// var resizeLocation = "views/public/webimages/";
+//
+// gm('images/IMG_0697.JPG')
+// .identify(function (err, data) {
+//   if (!err) console.log(data)
+// });
+//
 
 /*
 
@@ -45,8 +45,24 @@ function insertImgData(data){
     }
 });
 }
+*/
 
+/**
+ * Test code for creating clear square sized thumbnails.
+ *
+const width = 192;
+const height = 192;
+gm('md_IMG_0188.JPG')
+  .gravity('Center')
+  .crop(height, width)
+  .write('./tn_test2.JPG', (err) => {
+    if(err) {
+      console.log(err);
+    }
+  });
+*/
 
+/*
 for(let i = 0; i < files.length; i++) {
   gm('images/' + files[i]).size(function(err, value) {
     if(value.width > 5000) {
@@ -63,16 +79,16 @@ for(let i = 0; i < files.length; i++) {
 */
 
 // looks through images dir and creates a thumbnail @ 5%
-
-// for (var i = 0; i < files.length; i++) {
-//   gm('images/' + files[i])
-//   .resize(5,'%')
-//   .write(resizeLocation + 'tn_'+ files[i], function(err) {
-//     // if(!err) return console.dir(arguments)
-//     if(!err) return console.log(this.outname);
-//   });
-// };
-
+/*
+for (var i = 0; i < files.length; i++) {
+  gm('images/' + files[i])
+  .resize(5,'%')
+  .write(resizeLocation + 'tn_'+ files[i], function(err) {
+    // if(!err) return console.dir(arguments)
+    if(!err) return console.log(this.outname);
+  });
+};
+*/
 
 // looks through images dir and creates a medium size image @ 10%
 
